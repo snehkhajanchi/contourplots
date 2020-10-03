4278566c3d57b6e6d21dd1a43a4217b3a21af34a
snehkhajanchi <71020305+snehkhajanchi@users.noreply.github.com>
2020-10-03 14:23:25 +0530

    Update contourplots.py

--- contourplots.py
+++ contourplots.py
@@ -1,3 +1,12 @@
+from mpl_toolkits import mplot3d
+# Then, to create a 3D axes you can execute this code:
+<pre id="3346" class="graf graf--pre graf-after--p">%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = plt.axes(projection=’3d’)</pre>
+
+
 def f(x, y):
 
     return np.sin(np.sqrt(x ** 2 + y ** 2))
